d20ae426739f692a303fdfe24e8dd76d86be5308
novelshiffa <novelshiffa22@gmail.com>
2024-05-19 14:07:16 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var i int
+	var choice int
+	for {
+		fmt.Println("1. Product")
+		fmt.Println("2. Transaction")
+		fmt.Println("3. Exit")
+		fmt.Scanln(&choice)
+		if choice == 1 {
+			fmt.Println("Choice Product")
+		} else if choice == 2 {
+			fmt.Println("Choice Transaction")
+		} else if choice == 3 {
+			break
+		}
+	}
+}
