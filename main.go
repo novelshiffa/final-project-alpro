2c238f202bd3e2a1de950da7a7942227b4deeac2
fairuz <this.fairuz@gmail.com>
2024-05-19 14:12:39 +0700

    Add some spaces

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import "fmt"
 func main() {
 	// var i int
 	var choice int
+
 	for {
 		fmt.Println("1. Product")
 		fmt.Println("2. Transaction")
 		fmt.Println("3. Exit")
 		fmt.Scanln(&choice)
+
 		if choice == 1 {
 			fmt.Println("Choice Product")
 		} else if choice == 2 {
